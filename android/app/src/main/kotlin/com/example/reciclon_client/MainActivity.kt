094d670cb7ff88be18d821e710e6bc469d30098e
restoker <32400065+restoker@users.noreply.github.com>
2022-06-05 10:48:30 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/reciclon_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reciclon_client
+
+import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.engine.FlutterEngine;
+// import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+class MainActivity: FlutterFragmentActivity () {
+}
+// class MainActivity: FlutterActivity() {
+// }
